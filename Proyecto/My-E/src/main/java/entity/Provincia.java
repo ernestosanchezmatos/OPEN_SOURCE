01e324650e75606d3e2d84f78feceae479ac3ed8
ernestosanchezmatos <49891137+ernestosanchezmatos@users.noreply.github.com>
2019-10-08 21:33:53 -0500

    222

--- Proyecto/My-E/src/main/java/entity/Provincia.java
+++ Proyecto/My-E/src/main/java/entity/Provincia.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="Provincia")
-public class Provincia implements Serializable{
+public class Provincia implements SeriaSlizable{
 
 	private static final long serialVersionUID = 1L;
 
