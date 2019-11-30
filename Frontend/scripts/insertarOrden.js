1b94d576aeff40b0535314daed99e966f7320bf6
Ernesto <ernesto.sm.95@hotmail.com>
2019-11-30 01:59:18 -0500

    gaaxd

---
+++ Frontend/scripts/insertarOrden.js
@@ -0,0 +1,10 @@
+` <tr>  
+<td class="radio inlineblock m-r-20">
+        <input type="radio"  name="inversionTipo" id="publico" class="with-gap" value="PUBLICA"  >
+        <label for="publico">        &nbsp                                                        
+        </label>
+    </td>                                                     
+    <td>Jr. GAAA 145</td>
+    <td>Jr. GAAA 145</td>
+    <td>955740898</td>
+</tr>`
