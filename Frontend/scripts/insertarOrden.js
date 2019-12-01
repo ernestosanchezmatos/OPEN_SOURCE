2671a30c1085a78ae18e63c633c3e22ec51416dd
Ernesto <ernesto.sm.95@hotmail.com>
2019-12-01 05:55:47 -0500

    frontinsertar

--- Frontend/scripts/insertarOrden.js
+++ Frontend/scripts/insertarOrden.js
@@ -12,7 +12,7 @@ function InsertarMain(){
         lstaIngenieros=data.data;
         console.log(lstaIngenieros);
         for (var i=0;i<lstaIngenieros.length;i++){
-            var cod_ingeniero=lstaIngenieros[i].persona.personaId;
+            var cod_ingeniero=lstaIngenieros[i].empleadoId;
             tablaListaIng.innerHTML+=
             `<tr>  
                <td class="radio inlineblock m-r-20">
@@ -35,7 +35,7 @@ function InsertarMain(){
         lstaHospitales=data.data;
         console.log(lstaHospitales);
         for (var i=0;i<lstaHospitales.length;i++){
-            var cod_hosp=lstaHospitales[i].persona.personaId;
+            var cod_hosp=lstaHospitales[i].clienteId;
             tablaListaHosp.innerHTML+=
             `<tr>  
                <td class="radio inlineblock m-r-20">
@@ -74,47 +74,62 @@ axios.get(ruta3)
 }
 
 function RegistrarOrden(){
+    
     var ruta='http://localhost:8081/ordenes';
-    var fechaGenerada=document.getElementById('fechaGenerada').value;
+    var fechaGenerada=document.getElementById('fechaGenerada').value; 
     var fechaEjecutada=document.getElementById('fechaEjecutada').value;
     var listaingenieros=document.querySelector('input[name="listaingenieros"]:checked').value;
-    var listaejemplares=document.querySelector('input[name="listaejemplares"]').value;
-    var listahospitales=document.querySelector('input[name="listahospitales"]').value;
- 
-    axios({
-        method:'post',
-        url:ruta,
-        data:{       
-            empleado:{
-                empeladoId:1
-            }  ,  
-            cliente:{
-                clienteId:1
-            }    ,
-            fechaGeneracion:'2019-10-10',
-            fechaEjecucion:'2019-10-10',
-            nservicio:'gaa',
-            ejemplar:{
-                ejemplarId:1
-            },
-            numPuntaje:1,
-        }
-        }).then(data=>{
-          window.location="./admin_view_service.html";
-     })
+    var listaejemplares=document.querySelector('input[name="listaejemplares"]:checked').value;
+    var listahospitales=document.querySelector('input[name="listahospitales"]:checked').value;      
+     axios({
+         method:'post',
+         url:ruta,
+         data:{       
+             empleado:{
+                 empleadoId:listaingenieros
+             }  ,  
+             cliente:{
+                 clienteId:listahospitales
+             }    ,
+             fechaGeneracion:fechaGenerada,
+             fechaEjecucion:fechaEjecutada,
+             nservicio:'gg2222',
+             ejemplar:{
+                 ejemplarId:listaejemplares
+             },
+             numPuntaje:1,
+         }
+         }).then(data=>{
+           window.location="./admin_view_service.html";
+      })
 }
 function SelEjemplar(){
-    var listaejemplares=document.querySelector('input[name="listaejemplares"]').value;
-    
+    var listaejemplares=document.querySelector('input[name="listaejemplares"]:checked').value;
+    var nombreEjemplar=document.getElementById('nombreEjemplar');
+    for(var i=0;i<lstaEjemplares.length;i++){
+        if (lstaEjemplares[i].ejemplarId==listaejemplares){
+            nombreEjemplar.value=lstaEjemplares[i].nejemplar;
+        }
+    }    
 }
 
 function SelHospital(){
-    var listahospitales=document.querySelector('input[name="listahospitales"]').value;
-  
+    var listahospitales=document.querySelector('input[name="listahospitales"]:checked').value;
+    var nombreHospital=document.getElementById('nombreHospital');
+    for(var i=0;i<lstaHospitales.length;i++){
+        if (lstaHospitales[i].clienteId==listahospitales){
+            nombreHospital.value=lstaHospitales[i].persona.npersona;
+        }
+    }
 }
 
-function SelIngeniero(){
-    var listaingenieros=document.querySelector('input[name="listaingenieros"]').value;
-   
+function SelIngeniero(){   
+    var listaingenieros=document.querySelector('input[name="listaingenieros"]:checked').value;    
+    var nombreIngeniero=document.getElementById('nombreIngeniero');
+    for(var i=0;i<lstaIngenieros.length;i++){
+        if (lstaIngenieros[i].empleadoId==listaingenieros){
+            nombreIngeniero.value=lstaIngenieros[i].persona.npersona;
+        }
+    }    
 }
 
